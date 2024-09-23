3d58f54e04fc307ac41699f5cd04962e32c0ddb3
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-09-24 00:04:42 +0300

    Feat: Fetching data and using types

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <h2>Product ID: {{ id }}</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quisquam
-      saepe dolores sunt labore.
-    </p>
+    <p>{{ product?.title }}</p>
+    <p>{{ product?.price }}</p>
+    <p>{{ product?.id }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Product } from "~/types";
+
+const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+
+const { data: product } = await useFetch<Product>(uri);
+
 definePageMeta({
   layout: "products",
 });
-const { id } = useRoute().params;
 </script>
-
-<style scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <h2>Products</h2>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { Product } from "~/types";
+
 definePageMeta({
   layout: "products",
 });
-</script>
 
-<style scoped></style>
+const { data: products } = await useFetch<Product[]>(
+  "https://fakestoreapi.com/products"
+);
+</script>
