aa1ae04e0ac5716be54ec6dcdd932027dd251934
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-09-24 21:59:11 +0300

    Feat: Create ProductCard component and use it in products page

---
+++ components/ProductCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card text-center">
+    <img :src="product.image" alt="product thumb" class="mx-auto h-32" />
+    <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
+    <NuxtLink :to="`/products/${product.id}`">
+      <p class="btn m-4">View Details</p>
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { Product } from "~/types";
+
+const { product } = defineProps<{ product: Product }>();
+</script>
